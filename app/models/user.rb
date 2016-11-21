33442b832c83c12ded36974cf9a8d7d8c0603916
Sampson Crowley <sampsonsprojects@gmail.com>
2016-11-21 16:05:15 -0700

    working scrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,18 @@
 class User < ApplicationRecord
-  before_save { email.downcase! }
+  attr_accessor :password_confirmation
+  before_save do
+    email.downcase!
+    self.password = SCrypt::Password.create(password)
+  end
+
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence:   true, length: { maximum: 255 },
                     format:     { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 
-  has_secure_password
-  validates :password, presence: true, length: {minimum: 8}
+  validates :password, presence: true, length: { minimum: 8 }
+  validate do
+    errors.add(:password, "passwords must match") if password != password_confirmation
+  end
 end
